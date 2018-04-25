139cda525ea3398e4bf51a014db7a86e29c96126
kayweng <kaylek207@gmail.com>
2018-04-25 18:41:28 +0800

    update api url

--- src/axios/axios_db.js
+++ src/axios/axios_db.js
@@ -2,13 +2,14 @@ import axios from 'axios'
 import { store } from 'src/store/index'
 
 const aws = axios.create({
-  baseURL: 'https://nyandnoemj.execute-api.ap-southeast-1.amazonaws.com/Stage/',
+  baseURL: 'https://toy0dek93c.execute-api.ap-southeast-1.amazonaws.com/Development/',
   timeout: 30000
 })
 
 // interceptors setup
 aws.interceptors.request.use(config => {
   config.headers.common['Authorization'] = store.state.cognito.user.tokens.IdToken
+  console.log(config.headers)
   return config
 }, (error) => {
   return error
